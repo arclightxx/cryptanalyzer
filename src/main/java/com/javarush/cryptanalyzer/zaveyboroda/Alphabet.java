f35ddba576c40438f2ace87b7b4ba5dda8b37df6
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-02 18:34:34 +0500

    Add Alphabet.java

---
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/Alphabet.java
@@ -0,0 +1,9 @@
+package com.javarush.cryptanalyzer.zaveyboroda;
+
+public class Alphabet {
+    private static final String lettersUpperCase = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String lettersLowerCase = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    private static final String numbers = "0123456789";
+    private static final String symbols = ".,\"':-!? ";
+    public static final String ALPHABET = lettersUpperCase + lettersLowerCase + numbers + symbols;
+}
