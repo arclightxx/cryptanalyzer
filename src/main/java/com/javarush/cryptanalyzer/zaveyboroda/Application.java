f3b8f9016320cba120c8258a9e8a1be7e28901e7
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-03 00:31:02 +0500

    Move user input getters to UserInput.java and Menu.java

--- src/main/java/com/javarush/cryptanalyzer/zaveyboroda/Application.java
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/Application.java
@@ -3,9 +3,11 @@ package com.javarush.cryptanalyzer.zaveyboroda;
 import java.util.*;
 
 public class Application {
+    private Menu menu;
+    private UserInput userInput;
     private CaesarCipher caesarCipher;
-    Map<Integer, String> modes = new HashMap<>();
-    {
+    public static Map<Integer, String> modes = new HashMap<>();
+    static {
         modes.put(1, "Шифрование");
         modes.put(2, "Расшифровка");
         modes.put(3, "Взлом");
@@ -13,37 +15,18 @@ public class Application {
     private int mode;
     private String inputFile;
 
+    public Application() {
+        menu = new Menu();
+    }
+
     public void run() {
-        printMenuMessage();
-
-        try (Scanner scanner = new Scanner(System.in)) {
-            setMode(scanner);
-            System.out.printf(Menu.MODE_INFO_MESSAGE + "\n", modes.get(mode));
-            setInputFile(scanner);
-        }
+        menu.displayMenu();
+        init();
     }
 
-    private void printMenuMessage() {
-        System.out.println(Menu.MENU_MESSAGE);
-        System.out.println();
-    }
-
-    private void setMode(Scanner scanner) {
-        try {
-            mode = scanner.nextInt();
-        } catch (InputMismatchException e) { // TODO Создать исключение, которое выбрасывается при вводе всех цифр, кроме 1, 2, 3
-            System.out.println("Похоже, что Вы ввели символ. Пожалуйста, используйте цифры 1, 2 и 3 для корректной работы программы");
-        }
-    }
-
-    private void setInputFile(Scanner scanner) {
-        try {
-            inputFile = scanner.next();
-            while (!inputFile.equals("")){
-                inputFile = scanner.nextLine();
-            }
-        } catch (Exception e) { // TODO Создать исключение, которое выбрасывается при некорректном вводе имени файла
-            System.out.println(e.getMessage());
-        }
+    private void init() {
+        userInput = menu.getUserInput();
+        mode = userInput.getMode();
+        inputFile = userInput.getFileInput();
     }
 }
