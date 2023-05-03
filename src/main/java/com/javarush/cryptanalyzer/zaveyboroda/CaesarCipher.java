f5439b14eb7d321483f26f66ffe97a8052fde566
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-03 06:43:51 +0500

    Add encode and decode methods

--- src/main/java/com/javarush/cryptanalyzer/zaveyboroda/CaesarCipher.java
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/CaesarCipher.java
@@ -1,23 +1,51 @@
 package com.javarush.cryptanalyzer.zaveyboroda;
 
-import java.io.BufferedReader;
+import java.io.*;
+import java.util.Random;
 
 public class CaesarCipher {
-    private final char[] alphabet = Alphabet.ALPHABET.toCharArray();
     private int shift;
 
-    public void encode(BufferedReader message) {
-        //        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
-//             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt")))) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                char[] charLine = line.toCharArray();
-//                for (int i = 0; i < charLine.length; i++)
-//                    if (charLine[i] == '«' || charLine[i] == '»') charLine[i] = '"';
-//                writer.write(charLine);
-//                writer.newLine();
-//            }
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
+    public int encode(String inputFile) {
+        shift = getRandomShift();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                char[] charLine = line.toCharArray();
+                for (int i = 0; i < charLine.length; i++) {
+                    int index = (Alphabet.ALPHABET.indexOf(charLine[i]) + shift) % Alphabet.ALPHABET.length();
+                    charLine[i] = Alphabet.ALPHABET.charAt(index);
+                }
+                writer.write(charLine);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return shift;
+    }
+
+    private int getRandomShift() {
+        return new Random().nextInt(Alphabet.ALPHABET.length()) + 1;
+    }
+
+    public void decode(String inputFile, int shift) {
+        this.shift = -shift;
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                char[] charLine = line.toCharArray();
+                for (int i = 0; i < charLine.length; i++) {
+                    int index = (Alphabet.ALPHABET.indexOf(charLine[i]) + this.shift + Alphabet.ALPHABET.length()) % Alphabet.ALPHABET.length();
+                    charLine[i] = Alphabet.ALPHABET.charAt(index);
+                }
+                writer.write(charLine);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
