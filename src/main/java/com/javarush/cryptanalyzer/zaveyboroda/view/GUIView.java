22028a17f4972cb408dc5566d92ce77afb633ab6
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-08 12:27:48 +0500

    Add GUIView.java

---
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/view/GUIView.java
@@ -0,0 +1,15 @@
+package com.javarush.cryptanalyzer.zaveyboroda.view;
+
+import com.javarush.cryptanalyzer.zaveyboroda.entities.UserInput;
+
+public class GUIView implements View {
+    @Override
+    public void displayMenu() {
+
+    }
+
+    @Override
+    public UserInput getUserInput() {
+        return null;
+    }
+}
