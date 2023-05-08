5401fac5cccdfb5bed1ba7036e5d5ed34ca18b1c
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-08 12:30:06 +0500

    Refactor Main.java using View and MainController

--- src/main/java/com/javarush/cryptanalyzer/zaveyboroda/Main.java
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/Main.java
@@ -1,8 +1,15 @@
 package com.javarush.cryptanalyzer.zaveyboroda;
 
+import com.javarush.cryptanalyzer.zaveyboroda.app.Application;
+import com.javarush.cryptanalyzer.zaveyboroda.controllers.MainController;
+import com.javarush.cryptanalyzer.zaveyboroda.view.ConsoleView;
+import com.javarush.cryptanalyzer.zaveyboroda.view.View;
+
 public class Main {
     public static void main(String[] args) {
-        Application application = new Application();
+        View view = new ConsoleView();
+        MainController mainController = new MainController(view);
+        Application application = new Application(mainController);
         application.run();
     }
 }
