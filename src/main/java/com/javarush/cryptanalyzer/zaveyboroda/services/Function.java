85202ce5d996e7f7d880cfdf777147444b713f3b
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-08 14:02:17 +0500

    Add services

---
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/services/Function.java
@@ -0,0 +1,7 @@
+package com.javarush.cryptanalyzer.zaveyboroda.services;
+
+import com.javarush.cryptanalyzer.zaveyboroda.entities.UserInput;
+
+public interface Function {
+    void execute(UserInput userInput);
+}
