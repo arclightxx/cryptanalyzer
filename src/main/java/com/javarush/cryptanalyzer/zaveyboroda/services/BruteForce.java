c109379f25e9d98274d921fe4967fc6d58e2912b
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-08 14:07:54 +0500

    Add BruteForce.java

---
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/services/BruteForce.java
@@ -0,0 +1,10 @@
+package com.javarush.cryptanalyzer.zaveyboroda.services;
+
+import com.javarush.cryptanalyzer.zaveyboroda.entities.UserInput;
+
+public class BruteForce implements Function {
+    @Override
+    public void execute(UserInput userInput) {
+
+    }
+}
