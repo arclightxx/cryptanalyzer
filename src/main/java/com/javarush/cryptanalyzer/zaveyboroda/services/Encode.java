3a1d0086783cdfaea735613e5c42bf79f5d19937
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-08 14:30:53 +0500

    Split encode and decode methods from CaesarCipher.java to execute methods of Encode.java and Decode.java

--- src/main/java/com/javarush/cryptanalyzer/zaveyboroda/services/Encode.java
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/services/Encode.java
@@ -1,10 +1,30 @@
 package com.javarush.cryptanalyzer.zaveyboroda.services;
 
+import com.javarush.cryptanalyzer.zaveyboroda.constants.Alphabet;
 import com.javarush.cryptanalyzer.zaveyboroda.entities.UserInput;
+
+import java.io.*;
+import java.nio.file.Path;
 
 public class Encode implements Function {
     @Override
     public void execute(UserInput userInput) {
-
+        int shift = userInput.getShift();
+        Path inputFile = userInput.getFileInput();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile.toFile()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                char[] charLine = line.toCharArray();
+                for (int i = 0; i < charLine.length; i++) {
+                    int index = (Alphabet.ALPHABET.indexOf(charLine[i]) + shift) % Alphabet.ALPHABET.length();
+                    charLine[i] = Alphabet.ALPHABET.charAt(index);
+                }
+                writer.write(charLine);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
