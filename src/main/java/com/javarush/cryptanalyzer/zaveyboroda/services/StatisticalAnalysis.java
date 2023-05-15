767ecca29ce148b17a8154304d82bbe506044b9b
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-15 23:21:36 +0500

    Clean test code in execute method

--- src/main/java/com/javarush/cryptanalyzer/zaveyboroda/services/StatisticalAnalysis.java
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/services/StatisticalAnalysis.java
@@ -9,33 +9,6 @@ import java.nio.file.Path;
 public class StatisticalAnalysis implements Function {
     @Override
     public void execute(UserInput userInput) {
-        Path inputFile = userInput.getFileInput();
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile.toFile()));
-             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
-            encode(reader, writer);
-            System.out.println("Зашифрованный текст находится в файле output.txt");
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
 
-    private void encode(BufferedReader reader, BufferedWriter writer) throws IOException {
-        String line;
-        while ((line = reader.readLine()) != null) {
-            char[] charLine = line.toCharArray();
-            for (int i = 0; i < charLine.length; i++) {
-                if (charLine[i] == '«' || charLine[i] == '»') {
-                    charLine[i] = '"';
-                }
-                if (charLine[i] == '–') {
-                    charLine[i] = '-';
-                }
-                if (charLine[i] == ' ') {
-                    charLine[i] = ' ';
-                }
-            }
-            writer.write(charLine);
-            writer.newLine();
-        }
     }
 }
