d54e9af488f51f1a5a9a457aca4fdfb342c7a06c
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-05-15 23:47:58 +0500

    Wrap app in cycle

--- src/main/java/com/javarush/cryptanalyzer/zaveyboroda/Main.java
+++ src/main/java/com/javarush/cryptanalyzer/zaveyboroda/Main.java
@@ -5,11 +5,21 @@ import com.javarush.cryptanalyzer.zaveyboroda.controllers.MainController;
 import com.javarush.cryptanalyzer.zaveyboroda.view.ConsoleView;
 import com.javarush.cryptanalyzer.zaveyboroda.view.View;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         View view = new ConsoleView();
         MainController mainController = new MainController(view);
         Application application = new Application(mainController);
-        application.run();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            application.run();
+            System.out.println("Вы хотите продолжить работу? Введите \"n\", чтобы выйти или нажмите ENTER, чтобы продолжить");
+            if (scanner.nextLine().equals("n")) {
+                scanner.close();
+                break;
+            }
+        }
     }
 }
